c0832b87cf9f24634ee5be360eb3672a46b37ddb
ghetiyabansi <bansip464@gmail.com>
2024-01-01 14:22:50 +0530

    Result commit

--- client/src/components/Gallery/Slogan.jsx
+++ client/src/components/Gallery/Slogan.jsx
@@ -4,7 +4,7 @@ import img2 from "../../assets/images/Gallery/Gallery_2.png";
 
 const Slogan = ({ name, position, comment }) => {
   return (
-      <div className="flex flex-col items-center justify-center p-5 md:p-10 m-2 md:m-4 rounded-5 text-center h-full">
+      <div className="flex flex-col items-center justify-center p-0 md:p-10 m-2 md:m-4 rounded-5 text-center h-full">
           <img className='w-[30px] h-[30px]' src={img1} />
           <p className="text-2xl md:text-3xl lg:text-4xl mb-2 md:mb-4 w-full md:w-1/2 lg:w-2/5 font-normal text-gray-500">{comment}</p>
           <p className="text-sm text-gray-500 w-full md:w-4/5">
--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -4,7 +4,7 @@ import ImageSlider from "../components/Gallery/ImageSlider";
 
 
 const Gallery = () => {
-  return (<div className="overflow-x-hidden">
+  return (<div className="overflow-x-hidden hero_section xl:h-[800px]">
     <Slogan name="John Doe" position="CEO" comment="A healthy start to every day! I heghly recommend these products, they do the trick"/>
     <ImageSlider />
   </div>
