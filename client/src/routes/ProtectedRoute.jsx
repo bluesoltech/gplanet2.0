43c4301247670e79208a234c7d2d852ef856f324
hardik <hardipsinhp780@gmail.com>
2024-01-12 19:33:09 +0530

    Ticket render

--- client/src/routes/ProtectedRoute.jsx
+++ client/src/routes/ProtectedRoute.jsx
@@ -4,14 +4,12 @@ import { authContext } from "../context/AuthContext";
 
 const ProtectedRoute = ({ children }) => {
   const { token } = useContext(authContext);
-  console.log(token);
 
   const accessibleRoute = token ? (
     children
   ) : (
     <Navigate to="/login" replace={true} />
   );
-  console.log(accessibleRoute);
 
   return accessibleRoute;
 };
