a2c504b32bc8d9b0dc753c3e47bebda1ec5f4cee
Rahul Dhavad <2104030100801@silveroakuni.ac.in>
2024-01-16 04:09:46 -0800

    final

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Form, Input, TextArea, Button } from 'semantic-ui-react';
 import emailjs from 'emailjs-com';
+import { FaSquareFacebook,FaSquareInstagram,FaSquareYoutube,FaSquareXTwitter} from "react-icons/fa6";
+import Swal from 'sweetalert2';
+import { Link } from 'react-router-dom';
+
+
 const SERVICE_ID = "service_vdl3qqo";
 const TEMPLATE_ID = "template_o1sa2dp";
 const PUBLIC_KEY = "YB-8iyD85SFekj-DS";
-import Swal from 'sweetalert2';
+
+
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
@@ -102,71 +108,19 @@ const Contact = () => {
               </Button>
             </Form>
             <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-             <div className='mb-5 h-auto'> <a className="text-indigo-500 ">ask@greenplanetrun.com <br/></a>
+             <div className='mb-5 h-auto'> 
+             <a className="text-indigo-500 cursor-pointer" href="mailto:ask@greenplanetrun.com ?">ask@greenplanetrun.com</a>
               </div>
-              <span className="inline-flex">
-              <a className="text-gray-500">
-                    <svg
-                      fill="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                    </svg>
-                  </a>
-                  <a className="ml-4 text-gray-500">
-                    <svg
-                      fill="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                    </svg>
-                  </a>
-                  <a className="ml-4 text-gray-500">
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <rect
-                        width="20"
-                        height="20"
-                        x="2"
-                        y="2"
-                        rx="5"
-                        ry="5"
-                      ></rect>
-                      <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                    </svg>
-                  </a>
-                  <a className="ml-4 text-gray-500">
-                    <svg
-                      fill="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                    </svg>
-                  </a>
+              <span className="inline-flex gap-3">
+           <Link to="https://www.facebook.com/GreenPlanetRunAhmedabad/">  <FaSquareFacebook className='w-7 h-7 '></FaSquareFacebook> </Link>
+           <Link to="https://www.instagram.com/green_planet_run/">   <FaSquareInstagram className='w-7 h-7'></FaSquareInstagram>     </Link>  
+           <Link to="https://twitter.com/i/flow/login?redirect_after_login=%2FGreenplanetrun" >   <FaSquareXTwitter className='w-7 h-7'></FaSquareXTwitter>   </Link> 
+           <Link to="https://www.youtube.com/@GreenPlanetRun" >   <FaSquareYoutube className='w-7 h-7'></FaSquareYoutube>   </Link>
               </span>
             </div>
           </div>
         </div>
-      </section>
+      </section >
     </div>
   );
 };
